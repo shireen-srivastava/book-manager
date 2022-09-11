2c7e4e7eff990b4da25636600469bf145ef56c88
shireen-srivastava <Shireen.srivastava.corestack.io>
2022-09-11 20:27:30 +0530

    addbook and login signup features added

--- book_manager/app.py
+++ book_manager/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, Response, json, redirect
+from flask import Flask, request, render_template, Response, json, redirect, flash
 from flask_pymongo import PyMongo
 from flask_bcrypt import bcrypt
 
@@ -7,61 +7,70 @@ app = Flask(__name__)
 app.config['MONGO_URI'] = "mongodb://localhost:27017/book_manager"
 mongo = PyMongo(app)
 
-#1 db with 2 collections
 db = mongo.db.users
 db1 = mongo.db.books
 
-@app.route("/login", methods = ["GET"])
+@app.route("/homepage", methods = ["GET"])
 def homepage():
-    new_user = []
-    for i in db.find():
-        new_user.append(i)
-    return render_template("index.html", newuser = new_user)
+    return render_template("index.html")
 
 
-@app.route("/registration", methods = ["POST", "GET"])
+@app.route("/registration", methods = ["GET","POST"])
 def createUsers():
-    # password = request.json['password']
-    # hash_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-    # id = db.insert_one({
-    #     'name': request.json['name'],
-    #     'email' : request.json['email'],
-    #     'password' : hash_password
-    # })
-    # print(id)
-    # return "Registration Successfull"
-
     if request.method == "POST":
 
         new_name = request.form['name']
         new_email = request.form['email']
         password = request.form['password']
-        hash_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
         id = db.insert_one({
             'name':new_name,
             'email' : new_email,
-            'password' : hash_password
+            'password' : password
         })
 
-        return redirect("/registration")
+        return redirect("/login")
     return render_template("register.html")
     
 
+@app.route("/login", methods = ["GET", "POST"])
+def getuser():
+    if request.method == "POST":
+        new_email = request.form['email']
+        password = request.form['password']
 
-@app.route("/books", methods = ["POST"])
+        res = db.find({"email": new_email, "password": password})
+        if len(list(res)) != 0:
+            return redirect("/books")
+        else:
+            print("Incorrect email id or password")
+            return redirect("/login")
+    return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    return redirect("/homepage")
+
+
+@app.route("/addbook", methods = ["GET", "POST"])
 def createBooks():
-    data = db1.insert_one({
-        'name' : request.json['name'],
-        'author' : request.json['author'],
-        'description' : request.json['description'],
-        'price' : request.json['price']
-    })
+    if request.method == "POST":
 
-    return Response(
-        mimetype = "application/json",
-        status = 201,
-        response = json.dumps({"message" : "Book details added successfully", "id" : str(data.inserted_id)})
-    )
+        name = request.form['name']
+        author = request.form['author']
+        description = request.form['description']
+        price = request.form['price']
+
+        id = db1.insert_one({
+            'name': name,
+            'author' : author,
+            'description' : description,
+            'price' : price
+        })
+
+        return redirect("/books")
+    return render_template("addbook.html")
+
 
 @app.route("/books", methods = ["GET"])
 def getbooks():
@@ -69,6 +78,7 @@ def getbooks():
     for i in db1.find():
         new_book.append(i)
     return render_template("book.html", newbook = new_book)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
